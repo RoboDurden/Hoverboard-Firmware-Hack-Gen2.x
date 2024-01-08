6515a94697ca45ebc4e2c52e345660efd04effd3
AILIFE4798 <142502122+AILIFE4798@users.noreply.github.com>
2024-01-08 16:49:35 +0800

    target.h change

--- HoverBoardGigaDevice/Inc/target.h
+++ HoverBoardGigaDevice/Inc/target.h
@@ -3,6 +3,8 @@
 	#define TARGET_nvic_irq_enable(a, b, c){nvic_irq_enable(a, b);}
 	#define TARGET_nvic_priority_group_set(a)	// that function does not exist for this target = not needed ?
 	#define TARGET_adc_vbat_disable()
+#elif defined MM32SPIN05	
+	#include "mm32_device.h"
 #else
 	#include "gd32f1x0.h"
 	
